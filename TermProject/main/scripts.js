d611f1b956c78596341b3cafd20daac1acf1adf3
banksss33 <super.b99@hotmail.com>
2022-11-14 18:47:41 +0700

    first commit

---
+++ TermProject/main/scripts.js
@@ -0,0 +1,3 @@
+import * as THREE from 'https://threejsfundamentals.org/threejs/resources/threejs/r132/build/three.module.js';
+import {OrbitControls} from 'https://threejsfundamentals.org/threejs/resources/threejs/r132/examples/jsm/controls/OrbitControls.js';
+import {GUI} from 'https://threejsfundamentals.org/threejs/../3rdparty/dat.gui.module.js';
